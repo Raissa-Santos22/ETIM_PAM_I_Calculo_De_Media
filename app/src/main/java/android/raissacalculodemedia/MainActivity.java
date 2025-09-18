a6e3be6c1e2cba4566e67d9054a58ac52651f036
Raissa Santos22 <raissa.santos171@etec.sp.gov.br>
2025-09-18 18:54:42 -0300

    Aplicativo finalizado

--- app/src/main/java/android/raissacalculodemedia/MainActivity.java
+++ app/src/main/java/android/raissacalculodemedia/MainActivity.java
@@ -1,6 +1,12 @@
 package android.raissacalculodemedia;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +16,81 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+   private EditText nota1, nota2, nota3, nota4, numeroFaltas;
+
+    private Button btncalcular;
+
+    private TextView resultado;
+
+    private String nome;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        initComponetes();
+
+        btncalcular.setOnClickListener(view ->{
+            validaCampos();
+            if(!(validaCampos())){
+                calcularMedia();
+            }else{
+                Toast.makeText(this, "Preencha todos os campos!", Toast.LENGTH_SHORT).show();
+            }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+
+
+    private void calcularMedia() {
+        double n1 = Double.parseDouble(nota1.getText().toString());
+        double n2 = Double.parseDouble(nota2.getText().toString());
+        double n3 = Double.parseDouble(nota3.getText().toString());
+        double n4 = Double.parseDouble(nota4.getText().toString());
+
+        double media = (n1 + n2 + n3 + n4)/4;
+
+        double faltas = Double.parseDouble(numeroFaltas.getText().toString());
+
+        if(media > 7){
+            if(faltas < 20){
+                resultado.setTextColor(Color.parseColor("#437845"));
+                resultado.setText("Aluno aprovado com media" + media);
+            } else{
+                resultado.setTextColor(Color.parseColor("#f44336"));
+                resultado.setText("Excesso de falta" + faltas);
+            }
+        }
+        else{
+            resultado.setTextColor(Color.parseColor("#F44336"));
+            resultado.setText("Aluno retido com média " + media);
+        }
+    }
+
+    private boolean validaCampos() {
+        return nota1.getText().toString().isEmpty()
+                && nota2.getText().toString().isEmpty()
+                && nota3.getText().toString().isEmpty()
+                && nota4.getText().toString().isEmpty();
+    }
+
+    @SuppressLint("WrongViewCast")
+    private void initComponetes() {
+        nota1 = findViewById(R.id.nota1);
+        nota2 = findViewById(R.id.nota2);
+        nota3 = findViewById(R.id.nota3);
+        nota4 = findViewById(R.id.nota4);
+        numeroFaltas = findViewById(R.id.numeroFaltas);
+        btncalcular = findViewById(R.id.btncalcular);
+        resultado = findViewById(R.id.resultado);
+
     }
 }
